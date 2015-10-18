4b22e4d6742aef383d6745cd1a20ae22e685604f
kolomakin@gmail.com <kolomakin@gmail.com>
2015-10-18 23:16:01 +0300

    search in bing for hello world

---
+++ SeleniumHelloWorld/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+
+            driver.Navigate().GoToUrl("http://bing.com/");
+            driver.Manage().Window.Maximize();
+
+            IWebElement sString = driver.FindElement(By.Id("sb_form_q"));
+            sString.SendKeys("Hello world");
+
+            IWebElement sSubmit = driver.FindElement(By.Id("sb_form_go"));
+            sSubmit.Click();
+
+            driver.Close();
+
+
+        }
+    }
+}
